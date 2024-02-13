d2bbd5a9b54d1c12d0e9b1284ab38f9588f049c5
Sheldon McGee <sheldon@tooshel.com>
2024-02-13 16:59:56 -0700

    the basic voting works, now gotta save to localstorage and hook in charts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,42 @@
 <script>
-
-    var bardata = [22, 1, 3, 5, 2, 33];
-
     import Chart from '../components/chart.svelte';
+
+    export let bardata = [22, 1, 3, 5, 2, 33];
+
+    export var titles = ['Mr Smith', 'Mrs. Krabapple', 'Miss Hoover', 'Principle Skinner', 'Maya'];
+
+   
+
+
+    function newvote(index) {
+        console.log('newvote', index);
+        bardata[index] = bardata[index] + 1;
+        bardata = bardata;
+    }
+
+    function clearvotes(index) {
+        console.log('clearvotes', index);
+        //bardata[index] = 0;
+        bardata = bardata;
+    }
+    var count = 0;
+
+
+    function addnew() {
+        console.log('addnew');
+        titles.push('Shelly Shel');
+        bardata.push(0);
+        titles = titles;
+        bardata = bardata;
+    }
+
+    function remove() {
+        console.log('remove');
+        titles.pop();
+        bardata.pop();
+        titles = titles;
+        bardata = bardata;
+    }
 
 </script>
 
@@ -18,90 +52,37 @@
 
        <Chart />
 
+       <!-- {@debug(titles)} -->
 
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3">
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text id="id-button1" x="50%" y="50%" fill="#eceeef" font dy=".3em">4</text></svg>
-            <div class="card-body">
-              <p class="card-text">Mr Smith</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" data-buttonnumber="1" class="btn btn-sm btn-primary btn-space votebutton">Vote!</button>
-                    <button type="button" data-buttonnumber="1" class="btn btn-sm btn-outline-secondary clearbutton">Clear</button>
-                </div>
-                <!-- <small class="text-body-secondary">9 mins</small> -->
-              </div>
-            </div>
-          </div>
+       <button on:click={() => (count += 1)}>
+            count: {count}
+        </button>
+
+       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-3">
+            {#each titles as name, index}
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text id="id-button1" x="50%" y="50%" fill="#eceeef" font dy=".3em">{bardata[index]}</text></svg>
+                            <div class="card-body">
+                            <p class="card-text">{name}</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-primary btn-space votebutton" on:click={() => newvote(index)}>Vote!</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary clearbutton" on:click={() => clearvotes(index)}>Clear</button>
+                                    </div>
+                                    <!-- <small class="text-body-secondary">9 mins</small> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            {/each}
         </div>
 
+        <button type="button" class="btn btn-primary btn-space m-1" on:click={addnew}>Add</button>
+        <button type="button" class="btn btn-primary btn-space m-1" on:click={remove}>Remove</button>
 
 
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text id="id-button2" x="50%" y="50%" fill="#eceeef" dy=".3em">0</text></svg>
-            <div class="card-body">
-              <p class="card-text">Mrs. Krabapple</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" data-buttonnumber="2" class="btn btn-sm btn-primary btn-space votebutton">Vote!</button>
-                    <button type="button" data-buttonnumber="2" class="btn btn-sm btn-outline-secondary clearbutton">Clear</button>
-                </div>
-                <!-- <small class="text-body-secondary">9 mins</small> -->
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text id="id-button3" x="50%" y="50%" fill="#eceeef" dy=".3em">0</text></svg>
-            <div class="card-body">
-              <p class="card-text">Miss Hoover</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" data-buttonnumber="3" class="btn btn-sm btn-primary btn-space votebutton">Vote!</button>
-                    <button type="button" data-buttonnumber="3" class="btn btn-sm btn-outline-secondary clearbutton">Clear</button>
-                </div>
-                <!-- <small class="text-body-secondary">9 mins</small> -->
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text  id="id-button4" x="50%" y="50%" fill="#eceeef" dy=".3em">0</text></svg>
-            <div class="card-body">
-              <p class="card-text">Principle Skinner</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" data-buttonnumber="4" class="btn btn-sm btn-primary btn-space votebutton">Vote!</button>
-                    <button type="button" data-buttonnumber="4" class="btn btn-sm btn-outline-secondary clearbutton">Clear</button>
-                </div>
-                <!-- <small class="text-body-secondary">9 mins</small> -->
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text id="id-button5" x="50%" y="50%" fill="#eceeef" dy=".3em">0</text></svg>
-            <div class="card-body">
-              <p class="card-text">Maya</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" data-buttonnumber="5" class="btn btn-sm btn-primary btn-space votebutton">Vote!</button>
-                    <button type="button" data-buttonnumber="5" class="btn btn-sm btn-outline-secondary clearbutton">Clear</button>
-                </div>
-                <!-- <small class="text-body-secondary">9 mins</small> -->
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-    </div>
   </div>
 
 </div>
