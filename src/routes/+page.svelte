91f5a441864975bbb4c324a975b2501e2ff984e8
Sheldon McGee <sheldon@tooshel.com>
2025-03-14 01:16:01 -0700

    fix to the navivation issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,7 +46,7 @@
 		console.log('editPollNameModal', editName);
 		showModal = true;
 	}
-	
+
 	function toggleButtons() {
 		bardata.toggleHideButtons();
 	}
@@ -109,11 +109,19 @@
 					d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
 				/><circle cx="12" cy="13" r="4" /></svg
 			>
-			<strong on:dblclick={toggleButtons} style="cursor: pointer;" title="Double-click to toggle buttons">{$bardata.pollname || 'Pop Poll'}</strong>
-			<button type="button" class="btn btn-sm btn-outline-light ms-2" class:hidden={$bardata.hideButtons} on:click={editPollNameModal}
-				><Pencil />
-			</button>
 		</a>
+		<strong
+			on:dblclick={toggleButtons}
+			style="cursor: pointer;"
+			title="Double-click to toggle buttons">{$bardata.pollname || 'Pop Poll'}</strong
+		>
+		<button
+			type="button"
+			class="btn btn-sm btn-outline-light ms-2"
+			class:hidden={$bardata.hideButtons}
+			on:click={editPollNameModal}
+			><Pencil />
+		</button>
 		<button
 			class="navbar-toggler"
 			type="button"
@@ -127,12 +135,6 @@
 		</button>
 	</div>
 </div>
-
-<style>
-	.hidden {
-		display: none !important;
-	}
-</style>
 
 <EditModal bind:showModal bind:editIndex bind:editName>
 	<h2 slot="header">
@@ -258,3 +260,9 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	.hidden {
+		display: none !important;
+	}
+</style>
